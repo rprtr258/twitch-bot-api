dc6e00eafea2852016717b482332b9a4e91e5234
rprtr258 <poletansky@gmail.com>
2022-03-15 22:08:03 +0300

    Remove unused errno

--- receiver/main.c
+++ receiver/main.c
@@ -2,9 +2,6 @@
 ssize_t read(int fd, void* buf, size_t count);
 ssize_t write(int fd, const void* buf, size_t count);
 
-#include <errno.h>
-//int errno;
-
 #include <stdio.h>
 int printf(const char *format, ...);
 
